75cba40bc113c4b37b343efe52440c963298ff35
mikerly131 <mrr5068@gmail.com>
2024-02-15 10:51:48 -0500

    appointment and base model build out - typing

--- models/appointment.py
+++ models/appointment.py
@@ -3,38 +3,48 @@ A booking of a healthcare event among patient(s), practitioner(s), related perso
 This may result in one or more Encounter(s).
 FHIR Version: R4
 """
-from base_model import Base, ResourceBase
+from base_model import Base
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
-from sqlalchemy import String, List, Enum
+from sqlalchemy import String, List, Integer, Text, Enum
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
-class Appointment(ResourceBase):
+class Appointment(Base):
     __tablename__ = 'appointment'
 
-    identifier: Mapped[int] = mapped_column(primary_key=True)
+    # Resource and Domain Resource FHIR data types. Resource type will always be "Appointment"
+    resource_type: Mapped[str] = mapped_column(String, default="Appointment")
+    id: Mapped[str] = mapped_column(String, primary_key=True)
+    display_text_status: Mapped[str] = mapped_column(String, nullable=False)
+    display_text_div: Mapped[str] = mapped_column(String, nullable=False)
+
+    # Appointment Resource FHIR data types: primitives
+    external_id: Mapped[int] = mapped_column(Integer)
     status: Mapped[str] = mapped_column(String)
-    cancelationReason: Mapped[str] = mapped_column(String)
-    serviceCategory: Mapped[str] = mapped_column(String)
-    serviceType: Mapped[str] = mapped_column(String)
-    speciality: Mapped[str] = mapped_column(String)
-    appointmentType: Mapped[str] = mapped_column(String)
-    reasonCode: Mapped[str] = mapped_column(String)
-    reasonReference: Mapped[str] = mapped_column(String)
     priority: Mapped[str] = mapped_column(String)
     description: Mapped[str] = mapped_column(String)
-    supportingInformation: Mapped[str] = mapped_column(String)
     start: Mapped[str] = mapped_column(String)
     end: Mapped[str] = mapped_column(String)
     minutesDuration: Mapped[str] = mapped_column(String)
-    slot: Mapped[str] = mapped_column(String)
     created: Mapped[str] = mapped_column(String)
     comment: Mapped[str] = mapped_column(String)
     patientInstruction: Mapped[str] = mapped_column(String)
-    basedOn: Mapped[str] = mapped_column(String)
-    requestedPeriod: Mapped[str] = mapped_column(String)
 
+    # Appointment Resource FHIR data types: concepts and references
+    cancelationReason: Mapped[str] = mapped_column(Text)
+    serviceCategory: Mapped[str] = mapped_column(Text)
+    serviceType: Mapped[str] = mapped_column(Text)
+    speciality: Mapped[str] = mapped_column(Text)
+    appointmentType: Mapped[str] = mapped_column(Text)
+    reasonCode: Mapped[str] = mapped_column(Text)
+    reasonReference: Mapped[str] = mapped_column(Text)
+    supportingInformation: Mapped[str] = mapped_column(Text)
+    slot: Mapped[str] = mapped_column(Text)
+    basedOn: Mapped[str] = mapped_column(Text)
+    requestedPeriod: Mapped[str] = mapped_column(Text)
+
+    # Appointment Resource FHIR data types: child resource (backbone element)
     participants: Mapped[List["AppointmentParticipant"]] = relationship(
         back_populates="appointment"
     )
@@ -43,12 +53,16 @@ class Appointment(ResourceBase):
 class AppointmentParticipant(Base):
     __tablename__ = 'appointment_participant'
 
-    type: Mapped[str] = mapped_column(String)
-    actor: Mapped[str] = mapped_column(String)
+    # Appointment Participant Resource FHIR data types: primitives
     required: Mapped[str] = mapped_column(String)
     status: Mapped[str] = mapped_column(String)
+
+    # Appointment Resource FHIR data types: concepts and references
+    type: Mapped[str] = mapped_column(Text)
+    actor: Mapped[str] = mapped_column(Text)
     period: Mapped[str] = mapped_column(String)
 
+    # Appointment Participant FHIR data types: parent resource (domain resource)
     appointment: Mapped["Appointment"] = relationship(back_populates="appointment_participant")
 
 
--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,11 @@ class Base(DeclarativeBase):
     pass
 
 
-class ResourceBase(Base):
-    id = Column(Integer, primary_key=True)
-    text_status = Column(String)
-    text_div = Column(String)
+# Not going to use this initially, seems too complicated
+# class ResourceBase(Base):
+#     __abstract__ = True
+#
+#     resource_type = Column(String)
+#     id = Column(Integer, primary_key=True)
+#     text_status = Column(String)
+#     text_div = Column(String)
